c82c7f6025e998a82e2f0d7627ffd2f5b0cd0e20
ahk1221 <ahmed16.ahk@gmail.com>
2019-11-29 02:56:21 +0500

    Update for Big Little update + SMLHelper v2

---
+++ ExteriorPlantPots/ExteriorPlantPotPrefab.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SMLHelper.V2.Assets;
+using SMLHelper.V2.Crafting;
+using SMLHelper.V2.Handlers;
+using UnityEngine;
+
+namespace ExteriorPlantPots
+{
+    public class ExteriorPlantPotPrefab : ModPrefab
+    {
+        private string basePlantPotPath;
+
+        public ExteriorPlantPotPrefab(string plantPotId, string displayName, string tooltip, string basePotPath, TechType basePotTechType) : base(plantPotId, $"Submarine/Build/{plantPotId}")
+        {
+            this.basePlantPotPath = basePotPath;
+
+            // Register the TechType
+            TechType = TechTypeHandler.AddTechType(plantPotId, displayName, tooltip, false);
+
+            // Register the Sprite(icon) of the new TechType with the icon of the base plant pot.
+            SpriteHandler.RegisterSprite(TechType, SpriteManager.Get(basePotTechType));
+
+            // Set the recipe of the TechType.
+            CraftDataHandler.SetTechData(TechType, new TechData()
+            {
+                craftAmount = 1,
+                Ingredients = new List<Ingredient>()
+                {
+                    new Ingredient(TechType.Titanium, 2)
+                }
+            });
+
+            // Add it as a buildable.
+            CraftDataHandler.AddBuildable(TechType);
+
+            // Add it to the group in the PDA.
+            CraftDataHandler.AddToGroup(TechGroup.ExteriorModules, TechCategory.ExteriorOther, TechType);
+
+            // Set it to unlock after the base TechType is unlocked.
+            KnownTechHandler.SetAnalysisTechEntry(basePotTechType, new List<TechType>() { TechType });
+
+            // Register this prefab.
+            PrefabHandler.RegisterPrefab(this);
+        }
+
+        public override GameObject GetGameObject()
+        {
+            var prefab = Resources.Load<GameObject>(basePlantPotPath);
+            var obj = GameObject.Instantiate(prefab);
+
+            var constructable = obj.GetComponent<Constructable>();
+            var techTag = obj.GetComponent<TechTag>();
+            var planter = obj.GetComponent<Planter>();
+            var prefabIdentifer = obj.GetComponent<PrefabIdentifier>();
+
+            constructable.techType = TechType;
+            constructable.allowedInBase = false;
+            constructable.allowedInSub = false;
+            constructable.allowedOutside = true;
+            constructable.allowedOnConstructables = false;
+            constructable.forceUpright = true;
+
+            planter.isIndoor = false;
+            planter.environment = Planter.PlantEnvironment.Dynamic;
+
+            techTag.type = TechType;
+            prefabIdentifer.ClassId = ClassID;
+
+            var largeWorldEntity = obj.AddComponent<LargeWorldEntity>();
+            largeWorldEntity.cellLevel = LargeWorldEntity.CellLevel.Global;
+
+            return obj;
+        }
+    }
+}
--- ExteriorPlantPots/Main.cs
+++ ExteriorPlantPots/Main.cs
@@ -1,6 +1,5 @@
 ﻿using UnityEngine;
 using SMLHelper;
-using SMLHelper.Patchers;
 
 namespace ExteriorPlantPots
 {
@@ -10,118 +9,29 @@ namespace ExteriorPlantPots
         public static string COMPOSITE_EXTERIOR_PLANT_POT_CLASSID = "CompositeExteriorPlantPot";
         public static string CHIC_EXTERIOR_PLANT_POT_CLASSID = "ChicExteriorPlantPot";
 
-        public static TechType basicExteriorPlantPotTechType;
-        public static TechType compositeExteriorPlantPotTechType;
-        public static TechType chicExteriorPlantPotTechType;
-
-        public static GameObject GetPotResource(string path, TechType techType, string classId)
-        {
-            var prefab = Resources.Load<GameObject>(path);
-            var obj = GameObject.Instantiate(prefab);
-
-            var constructable = obj.GetComponent<Constructable>();
-            var techTag = obj.GetComponent<TechTag>();
-            var planter = obj.GetComponent<Planter>();
-            var prefabIdentifer = obj.GetComponent<PrefabIdentifier>();
-
-            constructable.techType = techType;
-            constructable.allowedInBase = false;
-            constructable.allowedInSub = false;
-            constructable.allowedOutside = true;
-            constructable.allowedOnConstructables = false;
-            constructable.forceUpright = true;
-
-            planter.isIndoor = false;
-            planter.environment = Planter.PlantEnvironment.Dynamic;
-
-            techTag.type = techType;
-            prefabIdentifer.ClassId = classId;
-
-            var largeWorldEntity = obj.AddComponent<LargeWorldEntity>();
-            largeWorldEntity.cellLevel = LargeWorldEntity.CellLevel.Global;
-
-            return obj;
-        }
-
-        public static GameObject GetBasicPotResource()
-        {
-            return GetPotResource("Submarine/Build/PlanterPot", basicExteriorPlantPotTechType, BASIC_EXTERIOR_PLANT_POT_CLASSID);
-        }
-
-        public static GameObject GetCompositePotResource()
-        {
-            return GetPotResource("Submarine/Build/PlanterPot2", compositeExteriorPlantPotTechType, COMPOSITE_EXTERIOR_PLANT_POT_CLASSID);
-        }
-
-        public static GameObject GetChicPotResource()
-        {
-            return GetPotResource("Submarine/Build/PlanterPot3", chicExteriorPlantPotTechType, CHIC_EXTERIOR_PLANT_POT_CLASSID);
-        }
-
         public static void Patch()
         {
-            basicExteriorPlantPotTechType = TechTypePatcher.AddTechType(
+            var basicExteriorPlantPot = new ExteriorPlantPotPrefab(
                 BASIC_EXTERIOR_PLANT_POT_CLASSID,
                 "Basic Exterior Plant Pot",
-                "Titanium pot containing synthetic soil.");
+                "Titanium pot containing synthetic soil.",
+                "Submarine/Build/PlanterPot",
+                TechType.PlanterPot);
 
-            CustomPrefabHandler.customPrefabs.Add(new CustomPrefab(
-                BASIC_EXTERIOR_PLANT_POT_CLASSID,
-                "Submarine/Build/BasicExteriorPlantPot",
-                basicExteriorPlantPotTechType,
-                GetBasicPotResource));
-
-            CraftDataPatcher.customBuildables.Add(basicExteriorPlantPotTechType);
-            CraftDataPatcher.AddToCustomGroup(TechGroup.ExteriorModules, TechCategory.ExteriorOther, basicExteriorPlantPotTechType);
-
-            compositeExteriorPlantPotTechType = TechTypePatcher.AddTechType(
+            var compositeExteriorPlantPot = new ExteriorPlantPotPrefab(
                 COMPOSITE_EXTERIOR_PLANT_POT_CLASSID,
                 "Composite Exterior Plant Pot",
-                "Designer pot containing synthetic soil.");
+                "Designer pot containing synthetic soil.",
+                "Submarine/Build/PlanterPot2",
+                TechType.PlanterPot2);
 
-            CustomPrefabHandler.customPrefabs.Add(new CustomPrefab(
-                COMPOSITE_EXTERIOR_PLANT_POT_CLASSID,
-                "Submarine/Build/CompositeExteriorPlantPot",
-                compositeExteriorPlantPotTechType,
-                GetCompositePotResource));
-
-            CraftDataPatcher.customBuildables.Add(compositeExteriorPlantPotTechType);
-            CraftDataPatcher.AddToCustomGroup(TechGroup.ExteriorModules, TechCategory.ExteriorOther, compositeExteriorPlantPotTechType);
-
-            chicExteriorPlantPotTechType = TechTypePatcher.AddTechType(
+            var chicExteriorPlantPot = new ExteriorPlantPotPrefab(
                 CHIC_EXTERIOR_PLANT_POT_CLASSID,
                 "Chic Exterior Plant Pot",
-                "Upmarket pot containing synthetic soil.");
+                "Upmarket pot containing synthetic soil.",
+                "Submarine/Build/PlanterPot3",
+                TechType.PlanterPot3);
 
-            CustomPrefabHandler.customPrefabs.Add(new CustomPrefab(
-                CHIC_EXTERIOR_PLANT_POT_CLASSID,
-                "Submarine/Build/ChicExteriorPlantPot",
-                chicExteriorPlantPotTechType,
-                GetChicPotResource));
-
-            CraftDataPatcher.customBuildables.Add(chicExteriorPlantPotTechType);
-            CraftDataPatcher.AddToCustomGroup(TechGroup.ExteriorModules, TechCategory.ExteriorOther, chicExteriorPlantPotTechType);
-
-            var basicPlantPotSprite = SpriteManager.Get(TechType.PlanterPot);
-            var compositePlantPotSprite = SpriteManager.Get(TechType.PlanterPot2);
-            var chicPlantPotSprite = SpriteManager.Get(TechType.PlanterPot3);
-
-            CustomSpriteHandler.customSprites.Add(new CustomSprite(basicExteriorPlantPotTechType, basicPlantPotSprite));
-            CustomSpriteHandler.customSprites.Add(new CustomSprite(compositeExteriorPlantPotTechType, compositePlantPotSprite));
-            CustomSpriteHandler.customSprites.Add(new CustomSprite(chicExteriorPlantPotTechType, chicPlantPotSprite));
-
-            var techData = new TechDataHelper
-            {
-                _craftAmount = 1,
-                _ingredients = new System.Collections.Generic.List<IngredientHelper>()
-                {
-                    new IngredientHelper(TechType.Titanium, 2)
-                }
-            };
-
-            CraftDataPatcher.customTechData.Add(basicExteriorPlantPotTechType, techData);
-            CraftDataPatcher.customTechData.Add(compositeExteriorPlantPotTechType, techData);
-            CraftDataPatcher.customTechData.Add(chicExteriorPlantPotTechType, techData);
         }
     }
 }
